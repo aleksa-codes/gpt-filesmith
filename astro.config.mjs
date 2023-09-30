676526a36d110f619ec13df10f8fb239dfd3a7d0
aleksa-codes <stojsic.aleksa@gmail.com>
2023-09-30 13:36:31 +0200

    Astro 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [tailwind({
+    // config: { applyBaseStyles: false },
+  })]
+});

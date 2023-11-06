f73e6cb293b5838a0681c28c61e7d8aec98224a0
aleksa-codes <stojsic.aleksa@gmail.com>
2023-11-06 12:07:59 +0100

    Fix: dependencies upgrade and dynamic path improvements

--- src/pages/[...path].ts
+++ src/pages/[...path].ts
@@ -1,99 +1,87 @@
-import type { APIRoute } from "astro";
-import { Configuration, OpenAIApi } from "openai";
+import type { APIRoute } from 'astro';
+import OpenAI from 'openai';
 
-const configuration = new Configuration({
-  apiKey: import.meta.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({ apiKey: import.meta.env.OPENAI_API_KEY });
 
-function getContentType(extension: string | undefined) {
-  switch (extension) {
-    case "html":
-      return "text/html";
-    case "css":
-      return "text/css";
-    case "js":
-      return "application/javascript";
-    case "json":
-      return "application/json";
-    default:
-      return "text/plain";
-  }
-}
+const getContentType = (extension: string): string => {
+  const types: { [key: string]: string } = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/javascript',
+    json: 'application/json'
+  };
+  return types[extension] || (extension ? 'text/plain' : 'text/html');
+};
 
 function getMessagePrompt(extension: string | undefined, path: string | undefined) {
-  if (!extension || extension === "html") {
-    return `Create a HTML document with content that matches following URL path: ${path}. The website design and colors should match the path and be inspired by Dribbble. Minimum 100vh, responsive design, navigation, footer and multiple sections. Use only meta, title, and the provided Tailwind CSS script ("<script src='https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio'></script>"). For images use online sources that are topic-specific. No comments or extra tags:`;
-  } else if (extension === "json") {
-    return `Create a JSON structure with content that matches following URL path: ${path}. The structure should only contain relevant keys and values for the topic:`;
+  if (!extension || extension === 'html') {
+    return `Create a HTML document with content that matches following URL path: ${path}. The website design and colors should match the path and be inspired by Dribbble. Use Tailwind CSS. Minimum 100vh, responsive design, navigation, footer and multiple sections. For images use online sources that are topic-specific. No comments or extra tags.`;
+  } else if (extension === 'json') {
+    return `Create a JSON structure with content that matches following URL path: ${path}. The structure should only contain relevant keys and values for the topic.`;
   } else {
-    return `Create a file of type ${extension} with content that matches following URL path: ${path}:`;
+    return `Create a file of type ${extension} with content that matches following URL path: ${path}.`;
   }
 }
 
-async function generateCompletion(prompt: string) {
-  return openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+const generateCompletion = async (prompt: string) =>
+  openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
     messages: [
-      {
-        role: "system",
-        content: "You are an advanced AI developer that creates files.",
-      },
-      {
-        role: "user",
-        content: prompt,
-      },
+      { role: 'system', content: 'You are an expert AI developer that creates files.' },
+      { role: 'user', content: prompt }
     ],
-    temperature: 0.7,
-    top_p: 1,
-    presence_penalty: 0,
-    frequency_penalty: 0,
-    stop: ["</html>"],
+    temperature: 1.1,
+    stop: ['</html>']
   });
-}
 
-function validatePath(path: string) {
-  if (path.length > 100) {
-    throw new Error("Path is too long");
-  }
-}
+const validatePath = (path: string) => {
+  if (path.length > 100) throw new Error('Path too long');
+};
 
-export const get: APIRoute = async ({ params }) => {
-    const { path } = params;
-    validatePath(path as string);
-    const splittedPath = path?.split(".");
-    const extension = splittedPath!.length > 1 ? splittedPath?.pop() : "html";
-    const contentType = getContentType(extension);
-    const messagePrompt = getMessagePrompt(extension, path);
+export const GET: APIRoute = async ({ params }) => {
+  const { path = '' } = params;
+  validatePath(path);
 
-    const completion = await generateCompletion(messagePrompt);
+  // Get extension, if any
+  const extension = path.includes('.') ? path.split('.').slice(-1)[0] : '';
 
-    if (!completion) {
-      throw new Error("Completion not generated");
-    }
+  // Get content type
+  const contentType = getContentType(extension);
 
-    let content = completion.data.choices[0].message?.content;
+  // Generate the prompt
+  const prompt = getMessagePrompt(extension, path);
 
-    if (extension === "html") {
-      content = `
-        <div style="display: flex; flex-direction: column; align-items: center; justify-content: space-between; min-height: 100vh; background-color: #f1f1f1; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
-          <iframe src="data:text/html;charset=utf-8,${encodeURIComponent(content!)}" style="width: 100%; height: 90vh; border: none; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);"></iframe>
-          <a href="data:text/html;charset=utf-8,${encodeURIComponent(content!)}" download="${path?.split('.')[0]}.html" style="background-color: #6b46c1; color: white; font-weight: 600; padding: 0.5rem 1rem; border-radius: 0.375rem; text-decoration: none; outline: none; transition: background-color 0.3s, box-shadow 0.3s; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); margin-bottom: 1rem;">
-          Download
-        </a>
+  // Generate the completion
+  const { choices } = (await generateCompletion(prompt)) || {};
+
+  let content = choices?.[0]?.message?.content || '';
+
+  // Handle HTML-specific logic
+  if (extension === 'html' || extension === '') {
+    content = `
+    <body style="display: flex; flex-direction: column; height: 100vh; justify-content: space-between; background: #f2f2f2; font-family: Arial, sans-serif; margin: 0; padding: 0;">
+      <iframe style="flex: 1; width: 100%; border: none; overflow-y: auto;" src="data:text/html;charset=utf-8,${encodeURIComponent(
+        content!
+      )}"></iframe>
+      <div style="text-align: center; background: #f9f9f9; padding: 12px;">
+        <p style="font-size: 18px; font-weight: 500; color: #333; margin: 0;">Like what you see?</p>
+        <div style="display: inline-flex; gap: 8px;">
+          <a href="/" 
+             style="margin-top: 8px; background: #ccc; color: #333; font-weight: 600; padding: 8px 16px; border-radius: 12px; text-decoration: none; transition: background-color 0.3s ease;"
+             onmouseover="this.style.backgroundColor='#ddd'"
+             onmouseout="this.style.backgroundColor='#ccc'">
+            Go Home
+          </a>
+          <a href="data:text/html;charset=utf-8,${encodeURIComponent(content!)}" download="${path?.split('.')[0]}.html" 
+             style="margin-top: 8px; background: #6D28D9; color: #fff; font-weight: 600; padding: 8px 16px; border-radius: 12px; text-decoration: none; transition: background-color 0.3s ease;"
+             onmouseover="this.style.backgroundColor='#7C3AED'"
+             onmouseout="this.style.backgroundColor='#6D28D9'">
+            Download
+          </a>
+        </div>
       </div>
-      <style>
-        a:hover {
-          background-color: #5a3fbf;
-          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        }
-      </style>`;
+    </body>`;
   }
-  
-  return new Response(content, {
-    status: 200,
-    headers: {
-      "Content-Type": contentType,
-    },
-  });
+
+  return new Response(content, { status: 200, headers: { 'Content-Type': contentType } });
 };

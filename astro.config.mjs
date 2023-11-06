bb6a627385b1ab5fcc5b16528d56ff71ffff41eb
aleksa-codes <stojsic.aleksa@gmail.com>
2023-11-06 12:19:18 +0100

    Fix: cloudflare deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: cloudflare(),
   integrations: [tailwind({
     // config: { applyBaseStyles: false },
   })]

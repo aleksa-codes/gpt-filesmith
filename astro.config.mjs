07620233adc7bdd0cb758998dfbe5ef4047e1cc5
aleksa-codes <64015178+aleksa-codes@users.noreply.github.com>
2024-10-28 12:59:35 +0100

    fix: improve UI/UX and use astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 });
